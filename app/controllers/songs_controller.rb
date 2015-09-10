234568e33982bbfa1c25a9058665d14dde403252
Josh Mejia <joshua.mejia@gmail.com>
2015-09-10 11:43:01 -0600

    Add remaining CRUD to Song.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,45 @@
 class SongsController < ApplicationController
+  before_action :load_song, only: [:show, :edit, :update, :destroy]
+
   def index
     @songs = Song.all
   end
 
   def show
-    @song = Song.find(params[:id])
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    Song.create(song_params)
+    redirect_to songs_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @song.update(song_params)
+      redirect_to songs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song.delete
+    redirect_to songs_path
+  end
+
+  private
+
+    def song_params
+      params.require(:song).permit(:title, :artist)
+    end
+
+    def load_song
+      @song = Song.find(params[:id])
+    end
 end
